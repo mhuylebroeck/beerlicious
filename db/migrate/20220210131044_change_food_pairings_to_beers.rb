f0ae08220a6cccf9b8949ecdea911a7d63507f0e
mateo huylebroeck <yasserh@live.be>
2022-02-10 19:33:30 +0100

    footer added

--- db/migrate/20220210131044_change_food_pairings_to_beers.rb
+++
@@ -1,5 +0,0 @@
-class ChangeFoodPairingsToBeers < ActiveRecord::Migration[6.0]
-  def change
-     change_column :beers, :food_pairing, :string
-  end
-end
--- db/migrate/20220210133658_remove_food_pairing_to_beers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFoodPairingToBeers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :beers, :food_pairing, :string
-  end
-end
